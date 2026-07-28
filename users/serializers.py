cdbabf760fe2571da7bc5c4e0bfa2f1f4dd8f990
Farhan Ali <farhan314567@gmail.com>
2026-07-28 16:21:21 +0500

    Secure user registration role handling

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,26 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'role', 'phone']
+        fields = [
+            'id',
+            'username',
+            'email',
+            'password',
+            'phone'
+        ]
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            role=validated_data.get('role', 'guest'),
-            phone=validated_data.get('phone', '')
+            phone=validated_data.get('phone', ''),
+            role='guest'
         )
         return user
