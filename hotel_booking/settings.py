6391ba858e81f675caed184152349a6e0a162c16
Farhan Ali <farhan314567@gmail.com>
2026-09-10 01:13:38 +0500

    Add CSRF trusted origins

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -25,6 +25,9 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS", ""
+).split(",") if os.getenv("CSRF_TRUSTED_ORIGINS") else []
 
 # Application definition
 
